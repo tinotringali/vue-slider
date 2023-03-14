c7d71097db939aad389668e88748efbd07b84c93
tinotringali <tringali.tino94@gmail.com>
2023-03-14 23:10:55 +0100

    final commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+
+      img:'img/01.webp',
+
+      title:'Spiderman',
+
+      message: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum susciptempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis',
+
+      img2:'img/02.webp',
+      img3:'img/03.webp',
+      img4:'img/04.webp',
+      img5:'img/05.webp',
+    }
+  }
+}).mount('#app')
